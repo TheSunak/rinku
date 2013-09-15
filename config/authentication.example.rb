8f42a235b48a27bbe6fa37256d6735a94ae6418a
cjlwired <carlos.jose.lazo@gmail.com>
2013-09-14 22:21:09 -0400

    Update authentication example with exact ENV variable names.

--- config/authentication.example.rb
+++ config/authentication.example.rb
@@ -1,9 +1,8 @@
-# Set all consumer, authentication, and API keys or tokens here
-
+# Set all consumer, authentication, and API keys or tokens here.
 # You can now call ENV[<authentication_token>] anywhere in the Sinatra app.
 
-ENV['example_consumer_key']         = 'example_consumer_key_string_here'
-ENV['example_consumer_key_secret']  = 'example_consumer_key_secret_string_here'
+ENV['linkedin_api_key']       = 'example_linkedin_api_key_here'
+ENV['linkedin_secret_key']  	= 'example_linkedin_api_secret_key_here'
 
-ENV['example_auth_token']           = 'example_auth_token_string_here'
-ENV['example_auth_token_secret']    = 'example_auth_token_secret_string_here'
+ENV['linkedin_oauth_token']   = 'example_linkedin_oauth_token_here'
+ENV['linkedin_oauth_secret']  = 'example_linkedin_oauth_secret_token_here'
