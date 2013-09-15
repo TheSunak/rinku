91cb8494e22e1bed9aa6d4f2fc11e5058723d47a
cjlwired <carlos.jose.lazo@gmail.com>
2013-09-14 21:04:08 -0400

    Reorganize Linkedin API model.

--- lib/models/linkedin.rb
+++ lib/models/linkedin.rb
@@ -3,10 +3,12 @@ class Linkedin
 	@@consumer 		 = OAuth::Consumer.new(ENV['linkedin_api_key'], ENV['linkedin_secret_key'])
 	@@access_token = OAuth::AccessToken.new(@@consumer, ENV['linkedin_oauth_token'], ENV['linkedin_oauth_secret'])
 
-	def self.get_company_id(company_name)
-		body = access_token.get("http://api.linkedin.com/v1/company-search?keywords=#{company_name}.com&sort=relevance").body
-		hash = Hash.from_xml(body)
-		hash["company_search"]["companies"]["company"][0]["id"]
+	@@fields 			 = ["email-domains","company-type","website-url","industries","status","employee-count-range","locations:(contact-info:(phone1))"]
+
+	# Getter methods for class variables.
+
+	def self.fields
+		@@fields
 	end
 
 	def self.consumer
@@ -15,6 +17,20 @@ class Linkedin
 
 	def self.access_token
 		@@access_token
+	end
+
+	# Helper methods.
+
+	def self.field_string
+		"(" + fields.join(",") + ")"
+	end
+
+	# Search API for LinkedIn.
+
+	def self.get_company_id(company_name)
+		body = access_token.get("http://api.linkedin.com/v1/company-search?keywords=#{company_name}.com&sort=relevance").body
+		hash = Hash.from_xml(body)
+		hash["company_search"]["companies"]["company"][0]["id"]
 	end
 
 end
