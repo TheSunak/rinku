f179735f631ae8f3c4ad96833d0a341322a91cbd
Vishal Sunak <vishalsunak@gmail.com>
2013-09-17 10:47:05 -0400

    Delete authentication.example.rb

--- config/authentication.example.rb
+++
@@ -1,8 +0,0 @@
-# Set all consumer, authentication, and API keys or tokens here.
-# You can now call ENV[<authentication_token>] anywhere in the Sinatra app.
-
-ENV['linkedin_api_key']       = 'dfyl3q07hdu0'
-ENV['linkedin_secret_key']  	= 'cGSXANZ0E9ssOxTx'
-
-ENV['linkedin_oauth_token']   = 'd5e0cb3d-0561-4ee6-baea-899873a15dcf'
-ENV['linkedin_oauth_secret']  = 'acbcb52d-bef6-4577-9275-c60890f22c71'
