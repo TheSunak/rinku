38f007e6384eae294f5fb08aa608cd5df72833e2
Vishal Sunak <vishal@Vishals-MacBook-Pro.local>
2013-09-17 10:51:22 -0400

    fixed file structure

---
+++ config/authentication.example.rb
@@ -0,0 +1,8 @@
+# Set all consumer, authentication, and API keys or tokens here.
+# You can now call ENV[<authentication_token>] anywhere in the Sinatra app.
+
+ENV['linkedin_api_key']       = 'api_key'
+ENV['linkedin_secret_key']  	= 'api_secret'
+
+ENV['linkedin_oauth_token']   = 'oauth_token'
+ENV['linkedin_oauth_secret']  = 'oauth_secret'
